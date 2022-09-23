c5653ee6ef8fec7341ab6f02df620b172896bab4
tks18 <tksudharshan@gmail.com>
2022-09-23 07:27:57 +0530

    feat(plugins/db): initialize sequelize function with env vars

---
+++ src/plugins/db/db.ts
@@ -0,0 +1,50 @@
+import { Sequelize } from 'sequelize';
+import { dblog } from '@plugins/logger';
+import { InternalServerError } from '@plugins/errors';
+
+export const constructDBObject = () => {
+  const { DBHOST, DBPORT, DATABASENAME, DBUSERNAME, DBPASSWORD } = process.env;
+  if (DBHOST && DBPORT && DATABASENAME && DBUSERNAME && DBPASSWORD) {
+    return new Sequelize({
+      dialect: 'postgres',
+      host: DBHOST,
+      port: Number(DBPORT),
+      database: DATABASENAME,
+      username: DBUSERNAME,
+      password: DBPASSWORD,
+      logging: dblog.debug.bind(dblog),
+    });
+  } else {
+    throw new InternalServerError(
+      'Env Variables (DBHOST, DBPORT, DATABASENAME, DBUSERNAME, DBPASSWORD) Required for Connecting to Database',
+      'DBConnectError',
+    );
+  }
+};
+
+export const sequelize = constructDBObject();
+
+/**
+ * Connects to Postgres Database using Sequelize and tries to Authenticate with DB
+ *
+ * @param {Sequelize} sequelize - Sequelize DB Object
+ */
+export async function authenticateDB(sequelize: Sequelize): Promise<void> {
+  try {
+    await sequelize.authenticate();
+  } catch (e) {
+    throw new InternalServerError(
+      `Error While Authenticating Database: ${String(e)}`,
+      'DBConnectError',
+    );
+  }
+}
+
+/**
+ * Closes the Sequelize Database Connection
+ *
+ * @param {Sequelize} sequelize - Sequelize Database Object
+ */
+export async function closeDB(sequelize: Sequelize): Promise<void> {
+  await sequelize.close();
+}
