d8b395987aeb3b6e3548a69c35d50a2414dc9272
tks18 <tksudharshan@gmail.com>
2022-09-23 07:25:44 +0530

    fix(app.ts): load env variables before any module imports

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 // Initialization
 import dotenv from 'dotenv';
 
-// Import Server
-import { ExpressServer } from '@plugins';
-
 // Load ENV Variables to the Process
 dotenv.config();
+
+// Import Server
+import { ExpressServer } from '@plugins';
 
 // Create Server and Start the Server
 const PORT = process.env.PORT || 3000;
