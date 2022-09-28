149549e98c09fda9904834ff52b1fc9ec5176e71
tks18 <tksudharshan@gmail.com>
2022-09-28 07:10:08 +0530

    feat(models): write a function to initialize all models in sequelize

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,47 @@
-// Here Goes Database Models
+import {
+  initCalendarMaster,
+  initExpenseCategoryMaster,
+  initExpenseMaster,
+  initIncomeCategoryMaster,
+  initIncomeMaster,
+  initInvestmentCategoryMaster,
+  initInvestmentMaster,
+  initBankMaster,
+  initCreditCardMaster,
+  initDebitCardMaster,
+  initEMIMaster,
+  initInsuranceMaster,
+  initAssetCategoryMaster,
+  initAssetMaster,
+} from './master';
+import type { Sequelize } from 'sequelize';
+
+const allFunctions: ((sequelize: Sequelize) => void)[] = [
+  initCalendarMaster,
+  initExpenseCategoryMaster,
+  initExpenseMaster,
+  initIncomeCategoryMaster,
+  initIncomeMaster,
+  initInvestmentCategoryMaster,
+  initInvestmentMaster,
+  initBankMaster,
+  initCreditCardMaster,
+  initDebitCardMaster,
+  initEMIMaster,
+  initInsuranceMaster,
+  initAssetCategoryMaster,
+  initAssetMaster,
+];
+
+/**
+ * Initializes all the Sequelize Models for Syncing with Database
+ *
+ * @param {Sequelize} sequelize - Sequelize Object
+ */
+export function initAllModels(sequelize: Sequelize) {
+  allFunctions.forEach((initFunc) => {
+    initFunc(sequelize);
+  });
+}
+
+export * from './master';
---
+++ src/models/master/index.ts
@@ -0,0 +1,6 @@
+export * from './calendar';
+export * from './income';
+export * from './expense';
+export * from './investment';
+export * from './asset';
+export * from './other';
