8599004741d5de8082b39ee80c2a69f3d1faab9e
tks18 <tksudharshan@gmail.com>
2022-09-29 07:14:29 +0530

    feat(models): write relationships for investment master table

--- src/models/master/investment/master.model.ts
+++ src/models/master/investment/master.model.ts
@@ -1,5 +1,10 @@
 import { Model, DataTypes } from 'sequelize';
-import { InvestmentCategoryMaster } from './category.model';
+import {
+  InvestmentCategoryMaster,
+  Incomes,
+  Investments,
+  MarketData,
+} from '@models';
 import type {
   Sequelize,
   InferAttributes,
@@ -7,14 +12,31 @@ import type {
   CreationOptional,
   ForeignKey,
   NonAttribute,
+  HasManyGetAssociationsMixin,
+  HasManyAddAssociationMixin,
+  HasManyAddAssociationsMixin,
+  HasManySetAssociationsMixin,
+  HasManyRemoveAssociationMixin,
+  HasManyRemoveAssociationsMixin,
+  HasManyHasAssociationMixin,
+  HasManyHasAssociationsMixin,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+  Association,
 } from 'sequelize';
 
 /**
  * @class Investment Master Table
  */
 export class InvestmentMaster extends Model<
-  InferAttributes<InvestmentMaster>,
-  InferCreationAttributes<InvestmentMaster>
+  InferAttributes<
+    InvestmentMaster,
+    { omit: 'incomes' | 'transactions' | 'marketRecords' }
+  >,
+  InferCreationAttributes<
+    InvestmentMaster,
+    { omit: 'incomes' | 'transactions' | 'marketRecords' }
+  >
 > {
   declare _id: CreationOptional<number>;
   declare name: string;
@@ -27,6 +49,64 @@ export class InvestmentMaster extends Model<
   declare updatedAt: CreationOptional<Date>;
 
   declare investmentCategory?: NonAttribute<InvestmentCategoryMaster>;
+
+  declare getIncomes: HasManyGetAssociationsMixin<Incomes>;
+  declare addIncome: HasManyAddAssociationMixin<Incomes, number>;
+  declare addIncomes: HasManyAddAssociationsMixin<Incomes, number>;
+  declare setIncomes: HasManySetAssociationsMixin<Incomes, number>;
+  declare removeIncome: HasManyRemoveAssociationMixin<Incomes, number>;
+  declare removeIncomes: HasManyRemoveAssociationsMixin<Incomes, number>;
+  declare hasIncome: HasManyHasAssociationMixin<Incomes, number>;
+  declare hasIncomes: HasManyHasAssociationsMixin<Incomes, number>;
+  declare countIncomes: HasManyCountAssociationsMixin;
+  declare createIncome: HasManyCreateAssociationMixin<Incomes, 'investment_id'>;
+
+  declare incomes?: NonAttribute<Incomes[]>;
+
+  declare getTransactions: HasManyGetAssociationsMixin<Investments>;
+  declare addTransaction: HasManyAddAssociationMixin<Investments, number>;
+  declare addTransactions: HasManyAddAssociationsMixin<Investments, number>;
+  declare setTransactions: HasManySetAssociationsMixin<Investments, number>;
+  declare removeTransaction: HasManyRemoveAssociationMixin<Investments, number>;
+  declare removeTransactions: HasManyRemoveAssociationsMixin<
+    Investments,
+    number
+  >;
+  declare hasTransaction: HasManyHasAssociationMixin<Investments, number>;
+  declare hasTransactions: HasManyHasAssociationsMixin<Investments, number>;
+  declare countTransactions: HasManyCountAssociationsMixin;
+  declare createTransaction: HasManyCreateAssociationMixin<
+    Investments,
+    'master_id'
+  >;
+
+  declare transactions?: NonAttribute<Investments[]>;
+
+  // Market Data
+  declare getMarketRecords: HasManyGetAssociationsMixin<MarketData>;
+  declare addMarketRecord: HasManyAddAssociationMixin<MarketData, number>;
+  declare addMarketRecords: HasManyAddAssociationsMixin<MarketData, number>;
+  declare setMarketRecords: HasManySetAssociationsMixin<MarketData, number>;
+  declare removeMarketRecord: HasManyRemoveAssociationMixin<MarketData, number>;
+  declare removeMarketRecords: HasManyRemoveAssociationsMixin<
+    MarketData,
+    number
+  >;
+  declare hasMarketRecord: HasManyHasAssociationMixin<MarketData, number>;
+  declare hasMarketRecords: HasManyHasAssociationsMixin<MarketData, number>;
+  declare countMarketRecords: HasManyCountAssociationsMixin;
+  declare createMarketRecord: HasManyCreateAssociationMixin<
+    MarketData,
+    'master_id'
+  >;
+
+  declare marketRecords?: NonAttribute<MarketData[]>;
+
+  declare static associations: {
+    incomes: Association<InvestmentMaster, Incomes>;
+    transactions: Association<InvestmentMaster, Investments>;
+    marketRecords: Association<InvestmentMaster, MarketData>;
+  };
 }
 
 /**
