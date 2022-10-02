bf3a244ebd042915f55886f8d0eebac42875b8dc
tks18 <tksudharshan@gmail.com>
2022-10-02 19:46:54 +0530

    refactor(plugins): fix exports

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,6 @@
-export { default as axios } from './axios';
+export * from './axios';
 export * from './server';
 export * from './logger';
 export * from './db';
 export * from './calendar-builder';
+export * from './jwt';
