f3500c22f58ad8ac8f2921b3febe4ff5bb781259
tks18 <tksudharshan@gmail.com>
2022-10-02 19:55:08 +0530

    feat(routes): add all base paths, add paths for handling 404 paths

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,11 @@
 // Initialization
 import express, { Request, Response } from 'express';
+import { verifyToken } from '@plugins/server/middlewares';
+
+// Routes
+import userPath from './user';
+import setupPath from './setup';
+import dataPath from './data';
 
 // HTTP Error Middlewares
 import { NotFound } from '@plugins/errors';
@@ -16,13 +22,27 @@ const router = express.Router();
 
 const { NODE_ENV } = process.env;
 
+router.use('/user', userPath);
+router.use('/setup', verifyToken, setupPath);
+router.use('/data', verifyToken, dataPath);
+
 // Respond with all the Endpoints
 router.post('/endpoints', (req: Request, res: Response) =>
   new EndpointGenerator(res, router).serve(),
 );
 
-// Serve 404 when path is not found
+// Serve 404 when path (POST) is not found
 router.post(/(\/.*)+/, (req, res): void => {
+  errorResponseHandler(res, new NotFound('404: Path not found'));
+});
+
+// Serve 404 when path (PATCH) is not found
+router.patch(/(\/.*)+/, (req, res): void => {
+  errorResponseHandler(res, new NotFound('404: Path not found'));
+});
+
+// Serve 404 when path (DELETE) is not found
+router.delete(/(\/.*)+/, (req, res): void => {
   errorResponseHandler(res, new NotFound('404: Path not found'));
 });
 
